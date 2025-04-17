87b4a94e05077b3686e6212f313bcda7e3ef5b28
asenetcky <asenetcky@gmail.com>
2025-04-16 21:30:39 -0400

    fix: save ames to global env

--- tidy-modeling-with-r/setup.R
+++ tidy-modeling-with-r/setup.R
@@ -21,6 +21,11 @@ read_ames <- function() {
   nanoparquet::read_parquet(ames_path)
 }
 
+ames <- read_ames()
+
+# data(ames)
+# ames <- ames %>% mutate(Sale_Price = log10(Sale_Price))
+
 set.seed(502)
 ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
 ames_train <- training(ames_split)
